b7dcb0a94cd08b14560347bda63e6acddbde5fc1
David Feldman <dodya.feldman@gmail.com>
2025-11-07 16:26:17 -0600

    Added non-recursive Cooley-Tukey algorithm, Fix fixed-point float data type

--- src/fft.rs
+++ src/fft.rs
@@ -1,12 +1,16 @@
 use crate::complex::{Cplx, PI};
 
+use fixed::FixedI16;
+use fixed::types::extra::U15;
+
+pub type Fix = FixedI16<U15>;
 // TODO make fft impl not copy data when recursing; a possible solution is to map indices
 // between iterations instead of copying the data each time.
 //
 // Ex: pass a fn(usize)->usize param and use it before indexing into slice,
 // will be |x| 2*x+1 or |x| 2*x depending on recusion. Also transmit length, this
 // might be complicated
-pub fn fft(a: &[Cplx]) -> Vec<Cplx> {
+pub fn copy_fft(a: &[Cplx]) -> Vec<Cplx> {
 	let n = a.len();
 	if n <= 1 {
 		return a.to_vec();
@@ -18,8 +22,8 @@ pub fn fft(a: &[Cplx]) -> Vec<Cplx> {
 	let a_evens: Vec<Cplx> = a.iter().step_by(2).copied().collect();
 	let a_odds: Vec<Cplx> = a.iter().skip(1).step_by(2).copied().collect();
 
-	let y_evens = fft(&a_evens);
-	let y_odds = fft(&a_odds);
+	let y_evens = copy_fft(&a_evens);
+	let y_odds = copy_fft(&a_odds);
 
 	let mut y = vec![Cplx::new(0f32, 0f32); n];
 
@@ -152,28 +156,99 @@ pub fn roots_of_unity_6(n: usize) -> Vec<Cplx> {
 	}
 	out
 }
-// TODO: Fix, because this totally doesn't work yet.
-// Need [0..=m] calculation for roots_of_unity(n), not
-// all of roots_of_unity(m). Not sure how to do that for recursive bisection.
-pub fn symmetry_abuse_roots_of_unity(n: usize) -> Vec<Cplx> {
-	if n < 8 {
-		return roots_of_unity_6(n);
+
+// TODO: memoize somehow. Fix a constant num samples and gen sinewave once.
+fn generate_sinewave(num_samples: usize) -> Vec<Fix> {
+	let mut sinewave = Vec::with_capacity(num_samples);
+	for i in 0..num_samples {
+		sinewave.push(Fix::from_bits(
+			(((i as f32 * std::f32::consts::TAU / num_samples as f32).sin()) * i32::MAX as f32)
+				as i16,
+		));
+	}
+	sinewave
+}
+
+/// Takes in imaginary slice as real and imaginary parts
+pub fn fft_inplace(fr: &mut [Fix], fi: &mut [Fix]) {
+	let num_samples: usize = fr.len();
+	let sinewave = generate_sinewave(num_samples);
+	let num_samples_m_1: usize = fr.len() - 1;
+	let log2_num_samples = num_samples.ilog2() as usize;
+	let shift_amt = 16i32 - log2_num_samples as i32;
+
+	let mut tr: Fix; // temporary storage for swapping
+	let mut ti: Fix;
+
+	// let mut i: usize; // indices being combined in Danielson-Lanczos
+	let mut j: usize;
+
+	let mut l: usize; // Length of intermediate FFTs
+	let mut k: isize; // Lookup trig values from sine table
+
+	let mut istep: usize; // Length of the FFT result when you combine
+
+	let mut wr: Fix; // Trigonometric values from lookup table
+	let mut wi: Fix;
+
+	let mut qr: Fix; // Temporary variables used during DL part of algorithm
+	let mut qi: Fix;
+
+	// m is one of indices being swapped
+	let mut mr: usize; // the other index being swapped
+	for m in 1..num_samples_m_1 {
+		mr = ((m >> 1) & 0x5555) | ((m & 0x5555) << 1);
+		// swap consecutive pairs
+		mr = ((mr >> 2) & 0x3333) | ((mr & 0x3333) << 2);
+		// swap nibbles ...
+		mr = ((mr >> 4) & 0x0F0F) | ((mr & 0x0F0F) << 4);
+		// swap bytes
+		mr = ((mr >> 8) & 0x00FF) | ((mr & 0x00FF) << 8);
+		// shift down mr
+		mr >>= shift_amt;
+		// don't swap that which has already been swapped
+		if mr <= m {
+			continue;
+		};
+		// swap the bit-reveresed indices
+		// TODO: maybe do mem swap?
+		tr = fr[m];
+		fr[m] = fr[mr];
+		fr[mr] = tr;
+		ti = fi[m];
+		fi[m] = fi[mr];
+		fi[mr] = ti;
 	}
 
-	let m = n >> 3;
+	l = 1;
+	k = log2_num_samples as isize - 1;
 
-	let eighth = roots_of_unity_6(m);
-	let mut out = Vec::with_capacity(n);
-	out.extend_from_slice(&eighth);
-	for i in (0..m).rev() {
-		out.push(Cplx::new(-eighth[i].im, -eighth[i].re));
-	}
-	for i in 0..m {
-		out.push(Cplx::new(eighth[i].im, -eighth[i].re));
-	}
-	for i in (0..m).rev() {
-		out.push(Cplx::new(-eighth[i].re, eighth[i].im));
-	}
+	while l < num_samples {
+		istep = l << 1;
+		for m in 0..l {
+			j = m << k;
+			// todo!();
+			wr = sinewave[j + num_samples / 4];
+			wi = -sinewave[j];
+			wr >>= 1;
+			wi >>= 1;
 
-	return out;
+			for i in (m..num_samples).step_by(istep) {
+				j = i + l;
+
+				tr = wr * fr[j] - wi * fi[j];
+				ti = wr * fi[j] + wi * fr[j];
+
+				qr = fr[i] >> 1;
+				qi = fi[i] >> 1;
+
+				fr[j] = qr - tr;
+				fi[j] = qi - ti;
+				fr[i] = qr + tr;
+				fi[i] = qi + ti;
+			}
+		}
+		k -= 1;
+		l = istep;
+	}
 }
--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ mod tests {
 
 	use super::parser::{parse, parse1};
 	use crate::complex::{Cplx, PI};
-	use crate::fft::{fft, *};
+	use crate::fft::{copy_fft, *};
 
 	// TODO: rename/remove irrelevant tests
 
@@ -54,66 +54,64 @@ mod tests {
 
 		let combination = |x: f32| {
 			// Kinda whatever random periodic function
-			5.0 + component(x, 2.0)
-				+ -1.0 * component(x, 9.0)
+			component(x, 2.0)
+				+ component(x, 7.0)
+				+ component(x, 9.0)
+				+ component(x, 17.0)
 				+ component(x, 37.0)
-				+ 5.0 * component(x, 17.0)
-				+ 105.3 * component(x, 7.0)
 		};
 		for i in 0..size {
 			cosine.push(Cplx::new(combination(i as f32), 0f32));
 		}
 
-		let frequencies = fft(&cosine);
-		dbg!(frequencies[0].abs());
-
-		assert!((frequencies[0].abs() / size as f32 - 5.0).abs() < 1e-4);
-		assert!((frequencies[2].abs() / size as f32 - 0.50).abs() < 1e-4);
-		assert!((frequencies[7].abs() / size as f32 - 52.65).abs() < 1e-4);
-		assert!((frequencies[9].abs() / size as f32 - 0.50).abs() < 1e-4);
-		assert!((frequencies[17].abs() / size as f32 - 2.50).abs() < 1e-4);
-		assert!((frequencies[37].abs() / size as f32 - 0.50).abs() < 1e-4);
-	}
-	//TODO: Benchamrk: )
-	#[test]
-	fn compare_roots_of_unity_impls() {
-		let n = 1 << 30; //Run with --release lmao
-		let roots1 = roots_of_unity_1(n);
-		let roots2 = roots_of_unity_2(n);
-		let roots3 = roots_of_unity_3(n);
-		let roots4 = roots_of_unity_4(n);
-		let roots5 = roots_of_unity_5(n);
-		let roots6 = roots_of_unity_6(n);
-
-		let roots = [roots1, roots2, roots3, roots4, roots5, roots6];
-
-		let mut tolerance = [[0.0; 6]; 6];
-
-		for i in 0..6 {
-			for j in 0..6 {
-				for k in 0..n / 2 {
-					let diff = (roots[i][k] - roots[j][k]).abs();
-					if diff > tolerance[i][j] {
-						tolerance[i][j] = diff;
-					}
-				}
+		let frequencies = copy_fft(&cosine);
+		for j in 0..size / 2 {
+			let r = frequencies[j].re;
+			let i = frequencies[j].im;
+			if r * r + i * i > 0.2 {
+				println!("frequency {j} had magnitude {:.4} ", r * r + i * i);
 			}
-		}
-		for i in 0..6 {
-			for j in 0..6 {
-				print!("{:.4} ", tolerance[i][j]);
-			}
-			println!();
 		}
 	}
 
 	#[test]
-	fn test_abuse_of_symmetry() {
-		let n = 1 << 10;
-		let roots_raw = roots_of_unity_6(n);
-		let roots_cooked = symmetry_abuse_roots_of_unity(n);
-		for i in 0..n {
-			assert!((roots_raw[i] - roots_cooked[i]).abs() < 1e-4);
+	fn decompose_cos_sum_inplace() {
+		let size = 1 << 11;
+		let mut re = Vec::<Fix>::with_capacity(size);
+		let mut im = Vec::<Fix>::with_capacity(size);
+
+		let component = |x: f32, freq: f32| (2.0 * PI * freq / size as f32 * x).cos();
+
+		let combination = |x: f32| {
+			// Kinda whatever random periodic function
+			component(x, 2.0)
+				+ component(x, 7.0)
+				+ component(x, 9.0)
+				+ component(x, 17.0)
+				+ component(x, 37.0)
+		};
+		for i in 0..size {
+			re.push(Fix::from_bits(
+				(combination(i as f32) * i16::MAX as f32 / 2.0) as i16,
+			));
+			im.push(Fix::ZERO);
 		}
+
+		fft_inplace(&mut re, &mut im);
+		for j in 0..size / 2 {
+			let r = re[j];
+			let i = im[j];
+			if r * r + i * i > 0.2 {
+				println!("frequency {j} had magnitude {:.4} ", r * r + i * i);
+			}
+		}
+	}
+
+	#[test]
+	fn test_fixed_point() {
+		assert_eq!(Fix::MIN.to_bits(), i16::MIN);
+		assert_eq!(Fix::MAX.to_bits(), i16::MAX);
+		assert_eq!(Fix::from_bits(0), Fix::ZERO);
+		// Takeaway: use .from_bits(i16) to turn any i16 with an "equivalent" float in [-1, 1)
 	}
 }
