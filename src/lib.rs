9be2d85a557b299daaba94641d55e6720b572a10
David Feldman <dodya.feldman@gmail.com>
2025-11-22 15:50:32 -0600

    cleaned up main

--- src/lib.rs
+++ src/lib.rs
@@ -157,3 +157,54 @@ pub mod lib {
 		draw_spectra(&spectra, ffts_per_second(samples_per_second, step_size));
 	}
 }
+
+// Paul's main.rs code:
+// let host = cpal::default_host();
+
+// #[cfg(not(target_os = "linux"))]
+// let device = host.default_output_device().unwrap();
+// #[cfg(target_os = "linux")]
+// let device = host
+// 	.output_devices()
+// 	.unwrap()
+// 	.find(|dev| dev.name().as_deref() == Ok("pipewire"))
+// 	.unwrap();
+
+// println!("using audio device named \"{}\"", device.name().unwrap());
+
+// let RiffWavePcm { samples_per_second, samples } = RiffWavePcm::parse(file).unwrap();
+
+// let config = StreamConfig {
+// 	channels: 1,
+// 	sample_rate: SampleRate(samples_per_second),
+// 	buffer_size: BufferSize::Default,
+// };
+
+// dbg!(&config);
+
+// let mut samples = &*Box::leak(samples); // ez borrow checker error fix
+
+// let is_done = &*Box::leak(Box::new(AtomicBool::new(false)));
+
+// let stream = device
+// 	.build_output_stream(
+// 		&config,
+// 		move |data: &mut [i16], _: &cpal::OutputCallbackInfo| {
+// 			if let Some((head, tail)) = samples.split_at_checked(data.len()) {
+// 				data.copy_from_slice(head);
+// 				samples = tail;
+// 			} else {
+// 				(&mut data[..samples.len()]).copy_from_slice(samples);
+// 				(&mut data[samples.len()..]).fill(0);
+// 				samples = &[];
+// 				(is_done).store(true, Ordering::Relaxed);
+// 			}
+// 		},
+// 		move |e| panic!("encountered error: {e}"),
+// 		None,
+// 	)
+// 	.unwrap();
+
+// stream.play().unwrap();
+
+// while !is_done.load(Ordering::Relaxed) {}
